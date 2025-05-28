65839e05ecb9960c5667f0db739a154cab78e380
Noble Eugene <nobleeugene2005@gmail.com>
2025-05-28 06:47:50 +0100

    zig 14 support

--- sandbox/build.zig
+++ sandbox/build.zig
@@ -3,7 +3,7 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{ .name = "sandbox", .root_source_file = b.path("main.zig") , .target = b.graph.host});
     const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run application");
-    const cairo = b.dependency("zig-cairo", .{}).module("cairo");
+    const cairo = b.dependency("zigcairo", .{}).module("cairo");
     cairo.linkSystemLibrary("cairo", .{});
     exe.linkLibC();
     exe.root_module.addImport("cairo", cairo);
